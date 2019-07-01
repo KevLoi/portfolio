cad61d24bf18d7f70888ed7a20211b2c05fcff53
Kevin Loi <kevin@murj.com>
2019-06-30 21:33:33 -0700

    Updates

---
+++ precache-manifest.2fcadbb357e4abeba16946d1bf02a879.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc95b3b3564a2f5fc34476735b6f7d6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ae8bdcae25827f0fa017",
+    "url": "/portfolio/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "673788ce1c952b8c2058",
+    "url": "/portfolio/static/css/main.622ade92.chunk.css"
+  },
+  {
+    "revision": "ae8bdcae25827f0fa017",
+    "url": "/portfolio/static/js/2.4e28dfec.chunk.js"
+  },
+  {
+    "revision": "673788ce1c952b8c2058",
+    "url": "/portfolio/static/js/main.3cf093b9.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "0cfffb3986b92335640a8df882213904",
+    "url": "/portfolio/static/media/Agile.0cfffb39.png"
+  },
+  {
+    "revision": "290ac31ceb2d76f6775076324a28d4ef",
+    "url": "/portfolio/static/media/AmeritechLogo.290ac31c.png"
+  },
+  {
+    "revision": "7f920e19f24e5a9450ca5e55c084c56b",
+    "url": "/portfolio/static/media/Bootstrap.7f920e19.png"
+  },
+  {
+    "revision": "f6eb49465a25af42dbd761118f8087e3",
+    "url": "/portfolio/static/media/CSS.f6eb4946.png"
+  },
+  {
+    "revision": "ebe880ba05add588e0518ad30d6fe1de",
+    "url": "/portfolio/static/media/Figma.ebe880ba.png"
+  },
+  {
+    "revision": "e009101b5d1bbda4237006e1732657fc",
+    "url": "/portfolio/static/media/HTML.e009101b.png"
+  },
+  {
+    "revision": "0358f32c6dafe76c201caea5f4a14cf5",
+    "url": "/portfolio/static/media/JSX.0358f32c.png"
+  },
+  {
+    "revision": "a0a9cfe832dc78ce27f8c26a6a758211",
+    "url": "/portfolio/static/media/JavaScript.a0a9cfe8.png"
+  },
+  {
+    "revision": "4a6b27f4a56e9e4d4e38b4c70bb0d935",
+    "url": "/portfolio/static/media/KevinLoiResume.4a6b27f4.pdf"
+  },
+  {
+    "revision": "ccd333e468309c8cacd637948b9a18be",
+    "url": "/portfolio/static/media/MongoDB.ccd333e4.png"
+  },
+  {
+    "revision": "d57ce3ad6cbeb4ea188f8bb2e9e7fcfc",
+    "url": "/portfolio/static/media/NoSQL.d57ce3ad.png"
+  },
+  {
+    "revision": "c502658a509d27b53679b3ef73c0d82f",
+    "url": "/portfolio/static/media/NodeJS.c502658a.png"
+  },
+  {
+    "revision": "ea92d6580dda0fbcf61a8ccb16e0d02a",
+    "url": "/portfolio/static/media/Postgresql.ea92d658.png"
+  },
+  {
+    "revision": "65ac5c1b202cacba07b3f7d189bc5997",
+    "url": "/portfolio/static/media/ReactJS.65ac5c1b.png"
+  },
+  {
+    "revision": "7f8b6138b6b5825c38a95e328a0d30c5",
+    "url": "/portfolio/static/media/StarvingSlug.7f8b6138.png"
+  },
+  {
+    "revision": "db95c7a5fdfea6778ae62adce7a5196a",
+    "url": "/portfolio/static/media/Swift.db95c7a5.png"
+  },
+  {
+    "revision": "000529be94acde0b8a9ddbe5f3dfb504",
+    "url": "/portfolio/static/media/WalkthruLogo.000529be.png"
+  },
+  {
+    "revision": "c77092d032eac48c9d679f1fc1a6e1fc",
+    "url": "/portfolio/static/media/Wordpress.c77092d0.png"
+  },
+  {
+    "revision": "879904cc4d683b226dcda0ea6c5ea48b",
+    "url": "/portfolio/static/media/aboutImg.879904cc.jpeg"
+  },
+  {
+    "revision": "73f02145916ce99aea96c81aeb3af534",
+    "url": "/portfolio/static/media/acsSite.73f02145.png"
+  },
+  {
+    "revision": "043ff19740577c2444280e958eadf210",
+    "url": "/portfolio/static/media/d3.043ff197.png"
+  },
+  {
+    "revision": "61db6ec0b440102b549e7b593c57f620",
+    "url": "/portfolio/static/media/emeraldBay.61db6ec0.jpeg"
+  },
+  {
+    "revision": "6856d6aedc2a3aaf9c724c05c20abb43",
+    "url": "/portfolio/static/media/murjLogo.6856d6ae.png"
+  },
+  {
+    "revision": "e7d69b831500ed6bb62648f5134244da",
+    "url": "/portfolio/static/media/orientationLogo.e7d69b83.png"
+  },
+  {
+    "revision": "67245958730a608ca34145a2c5097e11",
+    "url": "/portfolio/static/media/sankeyDiagram.67245958.png"
+  },
+  {
+    "revision": "245ad371370bcedea35f84cf6066a542",
+    "url": "/portfolio/static/media/tipsyTrials.245ad371.png"
+  }
+]);
